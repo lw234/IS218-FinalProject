5a09b30dd8ecdf5ce459a2e8ad43b0bd4e0bea82
Lynn <lynn291991@yahoo.com>
2014-12-03 20:27:33 -0500

    make change to the links

--- Index.php
+++ Index.php
@@ -1,5 +1,6 @@
 <?php
 $program = new program();
+
 class program{
 	
 	function __construct(){
@@ -20,7 +21,8 @@ class program{
 	}
 	
 	
-}	
+}
+	
 abstract class page{
 	
 	public $content;
@@ -63,17 +65,11 @@ class homepage extends page{
 		<h1> IS 218 Final Project </h1>
 		<h2>College Data Project</h2>
 		<h3>Directory</h3>
-		<a href = "index.php?page=pEnrollment">The highest enrollments.</a><br>
-		<a href = "index.php?page=pLiabilities">The highest total liabilities</a><br>
-		<a href = "index.php?page=pAssets">The highest net assets</a><br>
-		<a href = "index.php?page=pRevenue">The highest total revenue</a><br>
-		<a href = "index.php?page=pRPS">The highest total revenue per student</a><br>
-		<a href = "index.php?page=pAPS">The highest net assets per student</a><br>
-		<a href = "index.php?page=pLPS">The highest total liabilities per student</a><br>
-		<a href = "index.php?page=p5">Top Colleges</a><br>
-		<a href = "index.php?page=pState">Colleges in my state</a><br>
-		<a href = "index.php?page=pPiL">The largest increase in liabilities</a><br>
-		<a href = "index.php?page=pPiE">The largest increase in enrollment</a><br>
+		<a href = "index.php?page=Enrollment"> Colleges with the highest enrollments.</a><br>
+		<a href = "index.php?page=TLiabilities">Colleges with the highest total liabilities</a><br>
+		<a href = "index.php?page=NAssets">Colleges with the highest net assets</a><br>
+		<a href = "index.php?page=NAPS"> Colleges with the highest net assets per student</a><br>
+		<a href = "index.php?page=LIE">Colleges with the largest increase in enrollment between 2011 and 2010</a><br>
 		
 		';
 	}
