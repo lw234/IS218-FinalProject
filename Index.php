3e1d8ce44660ca283e88f506f934bff6907578cd
Lynn <lynn291991@yahoo.com>
2014-12-08 00:11:27 -0500

    Question 2 and 3 solution

--- Index.php
+++ Index.php
@@ -65,9 +65,9 @@ class homepage extends page{
 		<h1> IS 218 Final Project </h1>
 		<h2>College Data Project</h2>
 		<h3>Directory</h3>
-		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=Enrollment"> Colleges with the highest enrollments in 2011.</a><br>
-		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=TLiabilities">Colleges with the highest total liabilities</a><br>
-		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=NAssets">Colleges with the highest net assets</a><br>
+		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=Enrollment"> Colleges with the highest enrollments in 2011</a><br>
+		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=TLiabilities">Colleges with the highest total liabilities in 2011</a><br>
+		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=NAssets">Colleges with the highest net assets in 2011</a><br>
 		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=NAPS"> Colleges with the highest net assets per student</a><br>
 		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=LIE">Colleges with the largest increase in enrollment between 2011 and 2010</a><br>
 		
@@ -103,6 +103,100 @@ class Enrollment extends page{
 			$this->content .= "<tr>";
 			$this->content .= "<td>" . $rows['Name'] . "</td>";
 			$this->content .= "<td>" . $rows['EN2011'] . "</td>";
+			$this->content .= "</tr>";
+		}
+		
+		$this->content .= "</table>";
+		
+		$DBH = null;
+		}
+		catch(PDOException $e){
+			echo $e->getMessage();
+		}
+		
+	}
+}
+
+
+
+/* QUESTION NUMBER 2: Create a web page that that lists the colleges with the largest amount of total liabilities.
+
+ */
+
+class TLiabilities extends page{
+	
+	function get(){
+		
+		$host = "sql2.njit.edu";
+		$dbname = "lw234";
+		$user ="lw234";
+		$pass = 'QkuxTlWHS';
+		try{
+		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+		$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$STH = $DBH->query("SELECT college.Name, Finance.L2011 FROM college INNER JOIN Finance ON Finance.UID = college.UID ORDER BY Finance.L2011 DESC ");
+		
+		$this->content .= "<h1>Colleges with the highest total liabilities in 2011</h1><br>";
+		
+		$this->content .= "<table border = 2>";
+		$this->content .= "
+			<tr>
+				<th>College Name</th>
+				<th>Total Liablities</th>
+			</tr>
+		";
+		
+		while($rows = $STH->fetch()){
+			$this->content .= "<tr>";
+			$this->content .= "<td>" . $rows['Name'] . "</td>";
+			$this->content .= "<td>" . $rows['L2011'] . "</td>";
+			$this->content .= "</tr>";
+		}
+		
+		$this->content .= "</table>";
+		
+		$DBH = null;
+		}
+		catch(PDOException $e){
+			echo $e->getMessage();
+		}
+		
+	}
+}
+/* QUESTION NUMBER 3: Create a web page that that lists the colleges with the largest amount of total liabilities.
+
+ */
+
+class NAssets extends page{
+	
+	function get(){
+		
+		$host = "sql2.njit.edu";
+		$dbname = "lw234";
+		$user ="lw234";
+		$pass = 'QkuxTlWHS';
+		try{
+		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+		$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$STH = $DBH->query("SELECT college.Name, Finance.N2011 FROM college INNER JOIN Finance ON Finance.UID = college.UID ORDER BY Finance.N2011 DESC ");
+		
+		$this->content .= "<h1>Colleges with the highest net assets in 2011</h1><br>"; 
+		
+		
+		$this->content .= "<table border = 2>";
+		$this->content .= "
+			<tr>
+				<th>College Name</th>
+				<th>Total net assests</th>
+			</tr>
+		";
+		
+		while($rows = $STH->fetch()){
+			$this->content .= "<tr>";
+			$this->content .= "<td>" . $rows['Name'] . "</td>";
+			$this->content .= "<td>" . $rows['N2011'] . "</td>";
 			$this->content .= "</tr>";
 		}
 		
