a134238f16c5dfa97b9860e664534bd844862553
Lynn <lynn291991@yahoo.com>
2014-12-07 22:02:53 -0500

    Question one solution

--- Index.php
+++ Index.php
@@ -65,7 +65,7 @@ class homepage extends page{
 		<h1> IS 218 Final Project </h1>
 		<h2>College Data Project</h2>
 		<h3>Directory</h3>
-		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=Enrollment"> Colleges with the highest enrollments.</a><br>
+		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=Enrollment"> Colleges with the highest enrollments in 2011.</a><br>
 		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=TLiabilities">Colleges with the highest total liabilities</a><br>
 		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=NAssets">Colleges with the highest net assets</a><br>
 		<a href = "http://web.njit.edu/~lw234/IS218/FinalProject/Index.php?page=NAPS"> Colleges with the highest net assets per student</a><br>
@@ -79,30 +79,30 @@ class Enrollment extends page{
 	
 	function get(){
 		
-		$host = "sql1.njit.edu";
-		$dbname = ""; 
-		$user ="";
-		$pass = '';
+		$host = "sql2.njit.edu";
+		$dbname = "lw234";
+		$user ="lw234";
+		$pass = 'QkuxTlWHS';
 		try{
 		$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
 		$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
-		$STH = $DBH->query("SELECT UNITID,INSTNM FROM college");
+		$STH = $DBH->query("SELECT college.Name, EN2011 FROM enrollment INNER JOIN college ON enrollment.UID = college.UID ORDER BY enrollment.EN2011 DESC ");
 		
-		$this->content .= "<h1>Highest Enrollment</h1><h3>Top 50 in 2011</h3><br>";
+		$this->content .= "<h1>Highest College Enrollment in 2011</h1><br>";
 		
 		$this->content .= "<table border = 2>";
 		$this->content .= "
 			<tr>
-				<th>Name</th>
+				<th>College Name</th>
 				<th>Enrollment</th>
 			</tr>
 		";
 		
 		while($rows = $STH->fetch()){
 			$this->content .= "<tr>";
-			$this->content .= "<td>" . $rows['UNITID'] . "</td>";
-			$this->content .= "<td>" . $rows['INSTNM'] . "</td>";
+			$this->content .= "<td>" . $rows['Name'] . "</td>";
+			$this->content .= "<td>" . $rows['EN2011'] . "</td>";
 			$this->content .= "</tr>";
 		}
 		
